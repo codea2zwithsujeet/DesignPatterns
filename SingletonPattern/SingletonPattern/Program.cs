05a011014e27d65aa16a2d2e6cf0d8ab793d24e8
codea2zwithsujeet <codea2zwithsujeet@gmail.com>
2023-05-21 19:02:54 +0530

    Implemented singleton design pattern with comments

--- SingletonPattern/SingletonPattern/Program.cs
+++ SingletonPattern/SingletonPattern/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using SingletonPattern;
+
+Singleton singleton = Singleton.Instance;
+singleton.SomeData = "Hello, Singleton!";
+singleton.SomeMethod();
+
---
+++ SingletonPattern/SingletonPattern/Singleton.cs
@@ -0,0 +1,54 @@
+﻿//The Singleton design pattern is a creational design pattern that ensures a
+//class has only one instance and provides a global point of access to it.
+namespace SingletonPattern
+{
+    //The class Singleton is declared as sealed to prevent inheritance.
+    //This ensures that no other class can inherit from the Singleton class and
+    //create additional instances.
+    public sealed class Singleton
+    {
+        private static Singleton instance;
+        private static readonly object lockObject = new object();
+        private string someData;
+
+        // Private constructor to prevent instantiation from outside the class
+        private Singleton()
+        {
+        }
+
+        //The instance variable is declared as static.
+        //This means it belongs to the class itself rather than an instance of the class.
+        public static Singleton Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    //The lockObject is used to synchronize access to the instance creation code,
+                    //ensuring thread safety in a multi-threaded environment.
+                    lock (lockObject)
+                    {
+                        if (instance == null)
+                        {
+                            instance = new Singleton();
+                        }
+                    }
+                }
+                return instance;
+            }
+        }
+
+        // Other members and methods of the Singleton class can be added below
+        public string SomeData
+        {
+            get { return someData; }
+            set { someData = value; }
+        }
+
+        public void SomeMethod()
+        {
+            // Method implementation
+        }
+    }
+
+}
